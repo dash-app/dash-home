2f172db79b2bd4c4e9a35eca2207a4f065ef3641
Laica-Lunasys <laica.lunasys@gmail.com>
2020-07-24 17:47:15 +0900

    [WIP] Add frontend

---
+++ frontend/dash-home/src/components/basements/Dashboard.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Props { }
+interface State { }
+
+class Dashboard extends React.Component<Props, State> {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+};
+
+export default Dashboard;
