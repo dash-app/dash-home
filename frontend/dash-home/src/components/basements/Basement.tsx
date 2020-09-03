48a616f18277085df30d3d3bad0f55111179aff5
Laica-Lunasys <laica.lunasys@gmail.com>
2020-09-03 14:59:22 +0900

    Migrate to Class Component

--- frontend/dash-home/src/components/basements/Basement.tsx
+++ frontend/dash-home/src/components/basements/Basement.tsx
@@ -2,26 +2,45 @@ import * as React from 'react';
 
 // Navbar
 import Navigation from '../navbar/Navigation';
-import { useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
 import { Container } from '../atoms/Themed';
 import { Div } from '../atoms/Core';
+import { connect } from 'react-redux';
+import { ThemeAction } from '../../reducers/themes/type';
 
 interface Props {
   children: React.ReactNode,
+  theme: string
 }
 interface State { }
 
-const Basement: React.FC<Props> = ({ children }) => {
-  const theme = useSelector<any, string>((state) => state.themes.name);
+const mapStateToProps = (store: { themes: ThemeAction }) => {
+  return {
+    theme: store.themes.name,
+  }
+}
 
-  return (
-    <Div>
-      <Navigation theme={theme} roomName={"room"} />
-      <Container fluid>
-        {children}
-      </Container>
-    </Div>
-  );
+const mapDispatchToProps = (dispatch: Dispatch, Props: Props) => ({
+})
+
+class Basement extends React.Component<Props, State> {
+  static defaultProps: { theme: string; };
+  
+  render() {
+    console.log(this.props);
+    return (
+      <Div>
+        <Navigation theme={this.props.theme} roomName={"room"} />
+        <Container fluid>
+          {this.props.children}
+        </Container>
+      </Div>
+    )
+  }
+}
+
+Basement.defaultProps = {
+  theme: "CHEEKY_WHITE"
 };
 
-export default Basement;
+export default connect(mapStateToProps, mapDispatchToProps)(Basement);
