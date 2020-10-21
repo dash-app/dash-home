54491f8ea72a3fa0beb700ba8cbac82b00352323
Laica-Lunasys <laica.lunasys@gmail.com>
2020-10-21 11:45:57 +0900

    Migrate to Functional Component in Home

--- frontend/dash-home/src/pages/Home.tsx
+++ frontend/dash-home/src/pages/Home.tsx
@@ -1,29 +1,24 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Container } from 'react-bootstrap';
-import AppCard from '../components/cards/AppCard';
+import { Container } from 'react-bootstrap';
 import { Button } from '../components/atoms/Themed';
 import Basement from '../components/basements/Basement';
 
 interface Props {
   ctrl: any,
 }
-interface State { }
 
-class Home extends React.Component<Props, State> {
-  render() {
-    console.debug(this.props.ctrl)
-    return (
-      <Basement>
-        {/* Cards.... */}
-        <Container fluid="lg">
-          <Link to="/about">
-            <Button>About</Button>
-          </Link>
-        </Container>
-      </Basement>
-    );
-  }
-};
+const Home: React.FC<Props> = (props: Props) => {
+  return (
+    <Basement>
+      {/* Cards.... */}
+      <Container fluid="lg">
+        <Link to="/about">
+          <Button>About</Button>
+        </Link>
+      </Container>
+    </Basement>
+  )
+}
 
 export default Home;
