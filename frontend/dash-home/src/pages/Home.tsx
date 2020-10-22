6be44cfc7e596ced9bc1b284733ad23a6426bbdf
itigopasuta <toshi4202ike@gmail.com>
2020-10-22 13:43:52 +0900

    Add debug output

--- frontend/dash-home/src/pages/Home.tsx
+++ frontend/dash-home/src/pages/Home.tsx
@@ -1,14 +1,55 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Button } from '../components/atoms/Themed';
 import Basement from '../components/basements/Basement';
+import { ControllersResult, fetchControllers, Controller } from '../remote-go/Controller';
+import { Span } from '../components/atoms/Core';
 
 interface Props {
   ctrl: any,
 }
 
+
 const Home: React.FC<Props> = (props: Props) => {
+
+  // useStateで状態の保存場所を定義する 
+  // 参考：(https://ja.reactjs.org/docs/hooks-state.html)
+  //const [controllersResult, setControllers] = React.useState<ControllersResult | undefined>(undefined);
+  const [controllersResult, setControllers] = useState<ControllersResult | undefined>(undefined);
+
+  // useEffectでコンポーネントのレンダリング後にfetchControllersを叩いてAPIから取得する
+  // 参考：(https://ja.reactjs.org/docs/hooks-effect.html)
+  React.useEffect(() => {
+
+    //処理に成功するとcontrollersResultに値が入る
+    fetchControllers(setControllers);
+    
+  }, []);
+   // 第2引数に空の配列を渡してあげることで、
+   //stateが更新されてもレンダリングが走らないようにする。 (無限ループ対策)
+
+   
+   return (
+    <Basement>
+      <Container fluid="lg">
+
+        {/* controllersResultが正常な値の場合のみ処理を行う */}
+        {controllersResult?.error == null && !controllersResult?.controllers ? 
+        <p>hoge</p>:
+        <div>
+            {Object.values(controllersResult.controllers!).map((controller: Controller) => 
+              <div> 
+                <p>{controller.name}</p>
+              </div>
+            )}
+        </div>
+        }
+      </Container>
+    </Basement>
+  )
+
+
   return (
     <Basement>
       {/* Cards.... */}
@@ -16,6 +57,7 @@ const Home: React.FC<Props> = (props: Props) => {
         <Link to="/about">
           <Button>About</Button>
         </Link>
+          
       </Container>
     </Basement>
   )
