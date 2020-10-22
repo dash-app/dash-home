1403b0ca9760f9f85abf9066d27ae68c45c7cd7e
itigopasuta <toshi4202ike@gmail.com>
2020-10-22 14:08:44 +0900

    Add debug output レビュー修正

--- frontend/dash-home/src/pages/Home.tsx
+++ frontend/dash-home/src/pages/Home.tsx
@@ -10,40 +10,32 @@ interface Props {
   ctrl: any,
 }
 
-
 const Home: React.FC<Props> = (props: Props) => {
 
-  // useStateで状態の保存場所を定義する 
-  // 参考：(https://ja.reactjs.org/docs/hooks-state.html)
-  //const [controllersResult, setControllers] = React.useState<ControllersResult | undefined>(undefined);
+  // useStateで状態の保存場所を定義する。 参考：(https://ja.reactjs.org/docs/hooks-state.html)
   const [controllersResult, setControllers] = useState<ControllersResult | undefined>(undefined);
 
-  // useEffectでコンポーネントのレンダリング後にfetchControllersを叩いてAPIから取得する
-  // 参考：(https://ja.reactjs.org/docs/hooks-effect.html)
+  // useEffectでコンポーネントのレンダリング後にfetchControllersを叩いてAPIから取得する。 参考：(https://ja.reactjs.org/docs/hooks-effect.html)
+  // 第2引数に空の配列を渡してあげることで、stateが更新されてもレンダリングが走らないようにする。 (無限ループ対策)
   React.useEffect(() => {
-
-    //処理に成功するとcontrollersResultに値が入る
-    fetchControllers(setControllers);
-    
+    fetchControllers(setControllers);//処理に成功するとcontrollersResultに値が入る    
   }, []);
-   // 第2引数に空の配列を渡してあげることで、
-   //stateが更新されてもレンダリングが走らないようにする。 (無限ループ対策)
 
-   
-   return (
+  return (
     <Basement>
       <Container fluid="lg">
 
         {/* controllersResultが正常な値の場合のみ処理を行う */}
-        {controllersResult?.error == null && !controllersResult?.controllers ? 
-        <p>hoge</p>:
-        <div>
-            {Object.values(controllersResult.controllers!).map((controller: Controller) => 
-              <div> 
+        {controllersResult?.error == null && !controllersResult?.controllers ?
+          <p>hoge</p> :
+          <div>
+            {Object.values(controllersResult.controllers!).map((controller: Controller) =>
+              <div>
+                {/* 画面上に取得したデータを出力する。(デバッグ) */}
                 <p>{controller.name}</p>
               </div>
             )}
-        </div>
+          </div>
         }
       </Container>
     </Basement>
@@ -57,7 +49,7 @@ const Home: React.FC<Props> = (props: Props) => {
         <Link to="/about">
           <Button>About</Button>
         </Link>
-          
+
       </Container>
     </Basement>
   )
