a65e8b7b1a13b4c74a5594dd682ca62f6362808b
Laica-Lunasys <laica.lunasys@gmail.com>
2020-11-17 12:00:38 +0900

    Fix port resolution when REACT_APP_API_ADDRESS does not provided

--- frontend/dash-home/src/config/index.tsx
+++ frontend/dash-home/src/config/index.tsx
@@ -1,6 +1,6 @@
 const API_ADDRESS = process.env.REACT_APP_API_ADDRESS ? 
     `${window.location.protocol}//${process.env.REACT_APP_API_ADDRESS}` :
-    `${window.location.protocol}//${window.location.hostname}:13105`; // 13105 is default port of dash-home.
+    `${window.location.protocol}//${window.location.hostname}`;
 
 export {
     API_ADDRESS
