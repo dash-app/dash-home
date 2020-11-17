2e4b04e5b85a0ac7a0660ad77e369e9e1d0676df
Laica Lunasys <laica.lunasys@gmail.com>
2020-11-17 10:59:53 +0000

    Fix domain resolution

--- frontend/dash-home/src/config/index.tsx
+++ frontend/dash-home/src/config/index.tsx
@@ -1,6 +1,6 @@
-const API_ADDRESS = process.env.REACT_APP_API_ADDRESS ? 
+const API_ADDRESS = process.env.REACT_APP_API_ADDRESS ?
     `${window.location.protocol}//${process.env.REACT_APP_API_ADDRESS}` :
-    `${window.location.protocol}//${window.location.hostname}`;
+    `${window.location.protocol}//${window.location.host}`;
 
 export {
     API_ADDRESS
