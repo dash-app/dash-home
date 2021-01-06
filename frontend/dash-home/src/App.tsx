504870a03538aa0c5f718d87ad0724659ae79cda
Laica Lunasys <laica.lunasys@gmail.com>
2021-01-07 00:01:21 +0900

    [WIP] Add i18n translation

--- frontend/dash-home/src/App.tsx
+++ frontend/dash-home/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 // import Board from './components/basements/Board';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -28,28 +28,30 @@ const App: React.FC = () => {
   library.add(fab, fas, far);
 
   return (
-    <ThemeProvider>
-      <RoomProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/about" component={About} />
+    <Suspense fallback="">
+      <ThemeProvider>
+        <RoomProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/about" component={About} />
 
-            {/* Room */}
-            <Route exact path="/room/setup" component={RoomSetup} />
+              {/* Room */}
+              <Route exact path="/room/setup" component={RoomSetup} />
 
-            {/* Controllers */}
-            <Route exact path="/controllers" component={Controllers} />
-            <Route exact path="/controllers/new" component={NewController} />
-            <Route exact path="/controllers/:id" component={Controller} />
-            <Route exact path="/controllers/:id/edit" component={EditController} />
+              {/* Controllers */}
+              <Route exact path="/controllers" component={Controllers} />
+              <Route exact path="/controllers/new" component={NewController} />
+              <Route exact path="/controllers/:id" component={Controller} />
+              <Route exact path="/controllers/:id/edit" component={EditController} />
 
-            <Route status={404} component={NotFound} />
-          </Switch>
-        </Router>
-      </RoomProvider>
-    </ThemeProvider>
+              <Route status={404} component={NotFound} />
+            </Switch>
+          </Router>
+        </RoomProvider>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
--- frontend/dash-home/src/pages/RoomSetup.tsx
+++ frontend/dash-home/src/pages/RoomSetup.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Form, Modal } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import { Center, Div, H1, P } from '../components/atoms/Core';
 import { NotifyError } from '../components/atoms/Notify';
 import { Button, HR } from '../components/atoms/Themed';
@@ -9,6 +10,8 @@ interface Props {
 }
 
 const RoomSetup: React.FC<Props> = (props: Props) => {
+  const { t } = useTranslation();
+  
   var tmpRoomName = "";
   const [postResult, setPostResult] = React.useState<RoomResult | undefined>(undefined);
 
@@ -23,22 +26,22 @@ const RoomSetup: React.FC<Props> = (props: Props) => {
   return (
     <Div>
       <Center>
-        <H1 style={{ fontSize: "5vmax" }}>Hello!</H1>
+        <H1 style={{ fontSize: "5vmax" }}>{t("room.setup.title")}</H1>
         <HR />
-        {postResult?.error && <NotifyError title="Failed create Room" message={postResult.error.data.error} />}
-        <P style={{ fontSize: "1.4vmax", fontWeight: 100 }}>Please insert room name:</P>
+        {postResult?.error && <NotifyError title={t("room.error.create")} message={postResult.error.data.error} />}
+        <P style={{ fontSize: "1.4vmax", fontWeight: 100 }}>{t("room.setup.description")}</P>
         <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Control
-              placeholder="Bed Room"
-              aria-label="room name"
+              placeholder={t("room.setup.roomName.placeholder")}
+              aria-label={t("room.setup.roomName.ariaLabel")}
               required={true}
               disabled={postResult && (postResult.status !== "PENDING" && postResult.status !== "FAILED")}
               onChange={(e: any) => {
                 tmpRoomName = e.currentTarget.value;
               }}
             />
-            <Button type="submit" disabled={postResult && (postResult.status !== "PENDING" && postResult.status !== "FAILED")}>NEXT</Button>
+            <Button type="submit" disabled={postResult && (postResult.status !== "PENDING" && postResult.status !== "FAILED")}>{t("button.next")}</Button>
           </Form.Group>
         </Form>
       </Center>
@@ -51,13 +54,13 @@ const RoomSetup: React.FC<Props> = (props: Props) => {
         centered
       >
         <Modal.Header>
-          <Modal.Title><div style={{textAlign: "center"}}>Tada! <span role="img" aria-label="tada">🎉</span></div></Modal.Title>
+          <Modal.Title><div style={{textAlign: "center"}}>{t("room.setup.welcome.title")} <span role="img" aria-label="tada">🎉</span></div></Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div style={{ textAlign: "center" }}>
             <p style={{fontSize: "4rem"}}><span role="img" aria-label="cake">🎂</span></p>
-            <p style={{fontSize: "1.5rem", fontWeight: 600}}>Welcome to Dash-App!</p>
-            <Button href="/">Open Dashboard</Button>
+            <p style={{fontSize: "1.5rem", fontWeight: 600}}>{t("room.setup.welcome.description")}</p>
+            <Button href="/">{t("dashboard.open")}</Button>
           </div>
         </Modal.Body>
       </Modal>
