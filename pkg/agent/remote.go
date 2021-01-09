cf4f778b3b9e7aa7cca279d87dfb52dc381df21f
itigopasuta <toshi4202ike@gmail.com>
2021-01-09 11:33:12 +0900

    複数のデータをまとめて投げるように修正

--- pkg/agent/remote.go
+++ pkg/agent/remote.go
@@ -18,6 +18,11 @@ type IRCode struct {
 	Code []int `json:"code"`
 }
 
+type Payload struct {
+	Signal   []int `json:"signal"`
+	Interval int   `json:"interval,omitempty"`
+}
+
 func (as *agentService) SendIR(ctx context.Context, hex []*hex.HexCode) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
@@ -26,60 +31,60 @@ func (as *agentService) SendIR(ctx context.Context, hex []*hex.HexCode) error {
 		return errors.New("agent not initialized")
 	}
 
+	//もともとforがあった場所（temp)
+
 	// TODO: Request to Pigent (with context)
-	for _, code := range hex {
-		if code.Interval != 0 {
-			time.Sleep(time.Duration(code.Interval) * time.Millisecond)
-		}
 
-		// Get Agent from store (must be move...)
-		agent, err := as.store.Get()
-		if err != nil {
-			return err
-		}
-
-		// NOTE: Not working?
-		//if !agent.Online {
-		//	return errors.New("agent is not online")
-		//}
-
-		payload := &IRCode{
-			Code: aeha.SignalToCode(430, code.Code, 13300),
-		}
-
-		// Marshal codes
-		b, err := json.Marshal(payload)
-		if err != nil {
-			return err
-		}
-
-		// Call API
-		req, err := http.NewRequest(
-			"POST",
-			fmt.Sprintf("http://%s/api/v1/ir", agent.Address),
-			bytes.NewBuffer(b),
-		)
-		if err != nil {
-			return err
-		}
-
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		if resp.StatusCode != http.StatusOK {
-			var r map[string]string
-			body, _ := ioutil.ReadAll(resp.Body)
-			if err := json.Unmarshal(body, &r); err != nil {
-				return err
-			}
-			return errors.New(r["error"])
-		}
-		resp.Body.Close()
+	// Get Agent from store (must be move...)
+	agent, err := as.store.Get()
+	if err != nil {
+		return err
 	}
+
+	// NOTE: Not working?
+	//if !agent.Online {
+	//	return errors.New("agent is not online")
+	//}
+	var payload []Payload
+	for _, code := range hex {
+		payload = append(payload, Payload{
+			Signal:   aeha.SignalToCode(430, code.Code, 13300),
+			Interval: code.Interval,
+		})
+	}
+
+	// Marshal codes
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	// Call API
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("http://%s/api/v1/ir", agent.Address),
+		bytes.NewBuffer(b),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		var r map[string]string
+		body, _ := ioutil.ReadAll(resp.Body)
+		if err := json.Unmarshal(body, &r); err != nil {
+			return err
+		}
+		return errors.New(r["error"])
+	}
+	resp.Body.Close()
 
 	return nil
 }
