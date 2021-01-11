a5cb544d699c547b0be7d6cb31aaf3c7f9550c58
Laica Lunasys <laica.lunasys@gmail.com>
2021-01-11 17:52:59 +0900

    Fix indent (4 -> 2)

--- frontend/dash-home/src/components/atoms/Core.tsx
+++ frontend/dash-home/src/components/atoms/Core.tsx
@@ -3,80 +3,80 @@ import styled from 'styled-components';
 import { ThemeContext } from '../themes/ThemeProvider';
 
 const H1 = (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) => {
-    return (
-        <h1
-            {...props}
-            style={{
-                ...props.style,
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
-            }}>
-            {props.children}
-        </h1>
-    )
+  return (
+    <h1
+      {...props}
+      style={{
+        ...props.style,
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
+      }}>
+      {props.children}
+    </h1>
+  )
 }
 
 const H2 = (props: any) => {
-    return (
-        <h2
-            {...props}
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
-            }}>
-            {props.children}
-        </h2>
-    )
+  return (
+    <h2
+      {...props}
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
+      }}>
+      {props.children}
+    </h2>
+  )
 }
 
 const H3 = (props: any) => {
-    return (
-        <h3
-            {...props}
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
-            }}>
-            {props.children}
-        </h3>
-    )
+  return (
+    <h3
+      {...props}
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
+      }}>
+      {props.children}
+    </h3>
+  )
 }
 
 const P = (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) => {
-    return (
-        <p
-            {...props}
-            style={{
-                ...props.style,
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
-            }}>
-            {props.children}
-        </p>
-    )
+  return (
+    <p
+      {...props}
+      style={{
+        ...props.style,
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF"
+      }}>
+      {props.children}
+    </p>
+  )
 }
 
 const Div = (props: any) => {
-    return (
-        <div
-            style={{
-            }} {...props}>
-            {props.children}
-        </div>
-    )
+  return (
+    <div
+      style={{
+      }} {...props}>
+      {props.children}
+    </div>
+  )
 }
 
 const Span = (props: any) => {
-    return (
-        <span
-            {...props}
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF",
-                ...props.style
-            }}>
-            {props.children}
-        </span>
-    )
+  return (
+    <span
+      {...props}
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFFFFF",
+        ...props.style
+      }}>
+      {props.children}
+    </span>
+  )
 }
 
 const Center = (props: any) => {
-    const Div = styled.div`
+  const Div = styled.div`
         position: absolute;
         text-align: center;
         top: 50%;
@@ -84,21 +84,21 @@ const Center = (props: any) => {
         transform: translateY(-50%) translateX(-50%);
         -webkit-transform: translateY(-50%) translateX(-50%);
     `
-    return (
-        <Div
-            {...props}
-        >
-            {props.children}
-        </Div>
-    )
+  return (
+    <Div
+      {...props}
+    >
+      {props.children}
+    </Div>
+  )
 }
 
 export {
-    H1,
-    H2,
-    H3,
-    P,
-    Div,
-    Span,
-    Center
+  H1,
+  H2,
+  H3,
+  P,
+  Div,
+  Span,
+  Center
 };
--- frontend/dash-home/src/components/atoms/Themed.tsx
+++ frontend/dash-home/src/components/atoms/Themed.tsx
@@ -3,113 +3,113 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ThemeContext } from '../themes/ThemeProvider';
 
 import {
-    Button as BaseButton,
-    Spinner as BaseSpinner
+  Button as BaseButton,
+  Spinner as BaseSpinner
 } from 'react-bootstrap';
 
 // Button
 const Button = (props: any) => {
-    const theme = React.useContext(ThemeContext).theme
-    // provided current (for Button Group)
-    if (props.type === "radio") {
-        return (
-            <BaseButton
-                variant={theme === "CHEEKY_WHITE" ?
-                    props.selected && "primary" :
-                    props.selected && "gray"
-                }
-                // variant={theme === "CHEEKY_WHITE" ? "primary" : "light"}
-                type="button"
-                {...props}
-            >
-                {props.children}
-            </BaseButton >
-        )
-    } else {
-        return (
-            <BaseButton
-                variant={theme === "CHEEKY_WHITE" ? "primary" : "gray"}
-                {...props}
-                selected={false}
-            >
-                {props.children}
-            </BaseButton>
-        )
-    }
+  const theme = React.useContext(ThemeContext).theme
+  // provided current (for Button Group)
+  if (props.type === "radio") {
+    return (
+      <BaseButton
+        variant={theme === "CHEEKY_WHITE" ?
+          props.selected && "primary" :
+          props.selected && "gray"
+        }
+        // variant={theme === "CHEEKY_WHITE" ? "primary" : "light"}
+        type="button"
+        {...props}
+      >
+        {props.children}
+      </BaseButton >
+    )
+  } else {
+    return (
+      <BaseButton
+        variant={theme === "CHEEKY_WHITE" ? "primary" : "gray"}
+        {...props}
+        selected={false}
+      >
+        {props.children}
+      </BaseButton>
+    )
+  }
 }
 
 const Icon = (props: any) => {
-    return (
-        <FontAwesomeIcon
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFF"
-            }}
-            {...props}
-        >
-            {props.children}
-        </FontAwesomeIcon>
-    )
+  return (
+    <FontAwesomeIcon
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFF"
+      }}
+      {...props}
+    >
+      {props.children}
+    </FontAwesomeIcon>
+  )
 }
 
 const IconInvert = (props: any) => {
-    return (
-        <FontAwesomeIcon
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#FFF" : "#000"
-            }}
-            {...props}
-        >
-            {props.children}
-        </FontAwesomeIcon>
-    )
+  return (
+    <FontAwesomeIcon
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#FFF" : "#000"
+      }}
+      {...props}
+    >
+      {props.children}
+    </FontAwesomeIcon>
+  )
 }
 
 const Spinner = (props: any) => {
-    return (
-        <BaseSpinner
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFF",
-                border: "0.15em solid currentColor",
-                borderRightColor: "transparent"
-            }}
-            {...props}
-        >
-            {props.children}
-        </BaseSpinner>
-    )
+  return (
+    <BaseSpinner
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#000" : "#FFF",
+        border: "0.15em solid currentColor",
+        borderRightColor: "transparent"
+      }}
+      {...props}
+    >
+      {props.children}
+    </BaseSpinner>
+  )
 }
 
 const SpinnerInvert = (props: any) => {
-    return (
-        <BaseSpinner
-            style={{
-                color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#FFF" : "#000",
-                border: "0.15em solid currentColor",
-                borderRightColor: "transparent"
-            }}
-            {...props}
-        >
-            {props.children}
-        </BaseSpinner>
-    )
+  return (
+    <BaseSpinner
+      style={{
+        color: React.useContext(ThemeContext).theme === "CHEEKY_WHITE" ? "#FFF" : "#000",
+        border: "0.15em solid currentColor",
+        borderRightColor: "transparent"
+      }}
+      {...props}
+    >
+      {props.children}
+    </BaseSpinner>
+  )
 }
 
 const HR = (props: any) => {
-    return (
-        <hr 
-            style={{
-                borderColor: "#8080803A"
-            }}
-            {...props}
-        />
-    )
+  return (
+    <hr
+      style={{
+        borderColor: "#8080803A"
+      }}
+      {...props}
+    />
+  )
 }
 
 export {
-    Button,
-    Icon,
-    IconInvert,
-    Spinner,
-    SpinnerInvert,
-    HR,
+  Button,
+  Icon,
+  IconInvert,
+  Spinner,
+  SpinnerInvert,
+  HR,
 };
